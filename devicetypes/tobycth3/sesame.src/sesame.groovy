a985b3d4b38048dc9ec5848032d50a3e0086f266
Jaime Torres <jaime.torres@capitalone.com>
2018-10-20 22:41:42 -0400

    Fix ups

--- devicetypes/tobycth3/sesame.src/sesame.groovy
+++ devicetypes/tobycth3/sesame.src/sesame.groovy
@@ -11,12 +11,12 @@
 
  
 preferences {
-input(name: "apikey", type: "password", title: "API Key", required: "true", description: "A valid API token")
-input(name: "threshold", type: "number", title: "Lock timeout threshold", defaultValue: "1", required: "true", description: "Number of minutes")
-  }
+	input(name: "apikey", type: "password", title: "API Key", required: "true", description: "A valid API token")
+	input(name: "threshold", type: "number", title: "Lock timeout threshold", defaultValue: "1", required: "true", description: "Number of minutes")
+}
   
   
- metadata {
+metadata {
 	definition (name: "Sesame", namespace: "tobycth3", author: "Toby Harris") {
 		capability "Battery"
 		capability "Lock"
@@ -28,7 +28,7 @@ input(name: "threshold", type: "number", title: "Lock timeout threshold", defaul
 	}
 
 	
-tiles(scale: 2) {
+	tiles(scale: 2) {
 		multiAttributeTile(name:"toggle", type: "generic", width: 6, height: 4){
 			tileAttribute ("device.lock", key: "PRIMARY_CONTROL") {
                 attributeState "unknown", label:"unknown", action:"lock.lock", icon:"st.locks.lock.unknown", backgroundColor:"#ff3333", nextState:"locking"
@@ -67,19 +67,19 @@ tiles(scale: 2) {
 
 
 def installed() {
-  init()
-  }
+	init()
+}
 
 def updated() {
-  unschedule()
-  init()
-  }
+	unschedule()
+	init()
+}
   
 def init() {
 	log.info "Setting up Schedule (every 5 minutes)..."
-    login()
+	login()
 	runEvery1Minute(poll)
-  }
+}
 
 
 
@@ -91,10 +91,10 @@ def lock() {
 	runIn(lockdelay, doorlockcheck)
 
 	api('lock', '{"command":"lock"}') { resp ->
-	//	log.trace "Lock response $response.status $response.data"
+		//	log.trace "Lock response $response.status $response.data"
 
-	log.debug "Starting status check timer"
-	runIn(15, poll)		
+		log.debug "Starting status check timer"
+		runIn(15, poll)		
 	}
 }
 
@@ -106,10 +106,10 @@ def unlock() {
 	runIn(lockdelay, doorlockcheck)
 
 	api('unlock', '{"command":"unlock"}') { resp ->
-	//	log.trace "Unlock response $response.status $response.data"
+		//	log.trace "Unlock response $response.status $response.data"
 
-	log.debug "Starting status check timer"
-	runIn(15, poll)	
+		log.debug "Starting status check timer"
+		runIn(15, poll)	
 	}
 }
 
@@ -171,46 +171,57 @@ def poll(){
 	    return
     }
     
-	log.info "Executing 'poll' on deviceID"
-    
-    api('deviceID', []) { resp ->
-		log.trace "Device ID: ${resp.data[0].device_id}"
-	
-        //	log.trace "Nickname: ${resp.data.nickname}"
-		def new_nickname = resp.data[0].nickname
-		def old_nickname = device.currentValue("nickname")
-		def nickname_changed = new_nickname != old_nickname
-		sendEvent(name: 'nickname', value: new_nickname, displayed: nickname_changed, isStateChange: nickname_changed)	
+    def polldelay = threshold * 60000
+	if (!state.lastpoll) { state.lastpoll = now() }
+	if ((now() - state.lastpoll) >= polldelay) {
+        state.lastpoll = now()
 
- 	}
+        log.info "Executing 'poll' on deviceID"
 
-	log.info "Executing 'poll' on status"
+        api('deviceID', []) { resp ->
+            log.trace "Device ID: ${resp.data[0].device_id}"
 
-	api('status', []) { resp ->
-	
-		def is_locked = resp.data.locked
-		if (is_locked) { 
-        	locked() 
+            //	log.trace "Nickname: ${resp.data.nickname}"
+            def new_nickname = resp.data[0].nickname
+            def old_nickname = device.currentValue("nickname")
+            def nickname_changed = new_nickname != old_nickname
+            sendEvent(name: 'nickname', value: new_nickname, displayed: nickname_changed, isStateChange: nickname_changed)	
+
         }
-		else if (!is_locked) { 
-        	unlocked()
+
+        log.info "Executing 'poll' on status"
+
+        api('status', []) { resp ->
+
+            //	log.trace "Locked: ${resp.data.locked}"
+            def is_locked = resp.data.locked
+            if (is_locked) { 
+                locked() 
+            }
+            else if (!is_locked) { 
+                unlocked()
+            }
+            else {
+                doorlockcheck()
+            }
+
+        //	log.trace "API: ${resp.data.responsive}"
+            def new_api = resp.data.responsive
+            if (new_api) { api_ok() }
+            else { api_failed() }
+
+
+        //	log.trace "Battery: ${resp.data.battery}"
+            def new_battery = resp.data.battery
+            def old_battery = device.currentValue("battery")
+            def battery_changed = new_battery != old_battery
+            sendEvent(name: 'battery', value: new_battery, unit:"%", displayed: battery_changed, isStateChange: battery_changed)	
         }
-		else {
-			doorlockcheck()
-		}
-		
-	//	log.trace "API: ${resp.data.responsive}"
-		def new_api = resp.data.responsive
-		if (new_api) { api_ok() }
-		else { api_failed() }
-	
-		
-	//	log.trace "Battery: ${resp.data.battery}"
-		def new_battery = resp.data.battery
-		def old_battery = device.currentValue("battery")
-		def battery_changed = new_battery != old_battery
-		sendEvent(name: 'battery', value: new_battery, unit:"%", displayed: battery_changed, isStateChange: battery_changed)	
- 	}
+    } 
+    else {
+		log.debug "Rate limiting API call - not enough time has elapsed"
+		// runIn(polldelay, poll)
+	}
 }
   
   
